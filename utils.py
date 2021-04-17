314683adcfe9ea7c7bfbff50007da510b21f56e1
Tianyu Pang <pty17@mails.tsinghua.edu.cn>
2021-04-17 17:38:32 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -18,12 +18,6 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cifar10_mean = (0.4914, 0.4822, 0.4465) # equals np.mean(train_set.train_data, axis=(0,1,2))/255
 cifar10_std = (0.2471, 0.2435, 0.2616) # equals np.std(train_set.train_data, axis=(0,1,2))/255
 
-def normalise(x, mean=cifar10_mean, std=cifar10_std):
-    x, mean, std = [np.array(a, np.float32) for a in (x, mean, std)]
-    x -= mean*255
-    x *= 1.0/(255*std)
-    return x
-
 def pad(x, border=4):
     return np.pad(x, [(0, 0), (border, border), (border, border), (0, 0)], mode='reflect')
 
